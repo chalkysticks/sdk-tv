1da2ea060f316c69c6cff9ff1bfe4e52ff826344
Matt Kenefick <keneficksays@gmail.com>
2021-06-18 14:58:12 -0400

    Add some methods to v1 Schedule

--- dist/Collection/Schedule.js
+++ dist/Collection/Schedule.js
@@ -8,6 +8,40 @@ class CollectionSchedule extends sdk_core_1.CollectionBase {
         this.endpoint = 'tv/schedule';
         this.model = Schedule_1.default;
     }
+    getCurrentVideo() {
+        const timeData = this.getTimeData();
+        return this.at(timeData.index);
+    }
+    getSecondsSinceMidnight() {
+        const d1 = new Date();
+        const d2 = new Date();
+        d2.setHours(0, 0, 0, 0);
+        return (d1.getTime() - d2.getTime()) / 1000;
+    }
+    getTimeData() {
+        const now = this.getSecondsSinceMidnight();
+        let model;
+        let d = 0;
+        let n = 0;
+        let i = 0;
+        for (i; i < this.length; i++) {
+            model = this.at(i);
+            d = model.getDuration();
+            n += d;
+            if (n > now) {
+                break;
+            }
+        }
+        return {
+            duration: d,
+            index: i,
+            time: d - (n - now),
+        };
+    }
+    getTimeForCurrentVideo() {
+        const timeData = this.getTimeData();
+        return timeData.time;
+    }
 }
 exports.default = CollectionSchedule;
 //# sourceMappingURL=Schedule.js.map
--- src/Collection/Schedule.ts
+++ src/Collection/Schedule.ts
@@ -27,4 +27,73 @@ export default class CollectionSchedule extends CollectionBase {
      * @type ModelSchedule
      */
     public model: any = ModelSchedule;
+
+
+    // region: Getters
+    // ---------------------------------------------------------------------------
+
+    /**
+     * Get video at the current time
+     *
+     * @return ModelSchedule
+     */
+    public getCurrentVideo(): ModelSchedule {
+        const timeData = this.getTimeData();
+
+        return this.at(timeData.index) as ModelSchedule;
+    }
+
+    /**
+     * UTC seconds since midnight
+     *
+     * @return number
+     */
+    public getSecondsSinceMidnight(): number {
+        const d1: Date = new Date();
+        const d2: Date = new Date();
+
+        d2.setHours(0, 0, 0, 0);
+
+        return (d1.getTime() - d2.getTime()) / 1000;
+    }
+
+    /**
+     * Get video at the current time
+     *
+     * @return ModelSchedule
+     */
+    public getTimeData() {
+        const now: number = this.getSecondsSinceMidnight();
+        let model: ModelSchedule;
+        let d: number = 0;
+        let n: number = 0;
+        let i: number = 0;
+
+        // Iterate through collection
+        for (i; i < this.length; i++) {
+            model = this.at(i) as ModelSchedule;
+            d = model.getDuration();
+            n += d;
+
+            if (n > now) {
+                break;
+            }
+        }
+
+        return {
+            duration: d,
+            index: i,
+            time: d - (n - now),
+        }
+    }
+
+    /**
+     *
+     */
+    public getTimeForCurrentVideo(): number {
+        const timeData = this.getTimeData();
+        return timeData.time;
+    }
+
+    // endregion: Getters
 }
